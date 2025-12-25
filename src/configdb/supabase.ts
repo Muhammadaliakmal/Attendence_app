ca39767b40736759b7b90b14ffd0045017bdb2a0
0-Umer-0 <azizakmal575@gmail.com>
2025-12-25 15:41:21 +0500

    Initial commit

---
+++ src/configdb/supabase.ts
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+  const supabaseUrl = "https://ikkubxzshghcetzljgdc.supabase.com";
+  const supabaseAnonKey = "sb_publishable_o9GE3W3XleOkM6flVSyVdA_p8_6-d9m";
+
+   const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+  export default supabase;
